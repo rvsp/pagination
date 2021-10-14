9b97c402a907cafe3a89a27596fbae0abf7b8bff
Venkatasubramanian <pvenkat.rv@gmail.com>
2021-10-14 23:44:08 +0530

    Create index.js

---
+++ form/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-alert */
+const projectName = 'survey-form';
+console.log('Project', projectName);
+function submitForm() {
+  alert('The form was submitted');
+}
